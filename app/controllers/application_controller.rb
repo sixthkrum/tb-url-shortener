5142710cfbab44223f3544023d831fa77c78fe99
Vikram Alagh <mailbox.vikramalagh@gmail.com>
2025-02-07 14:55:58 +0530

    fix: send an error message signalling invalid parameters when strong parameters raise exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  rescue_from ActionController::ParameterMissing do |_|
+    error_message = "Invalid parameters passed"
+    respond_to do |format|
+      format.json do
+        render json: { data: {}, errors: [ { message: error_message } ] }, status: :bad_request
+      end
+
+      format.html do
+        render plain: error_message, status: :bad_request
+      end
+    end
+  end
 end
